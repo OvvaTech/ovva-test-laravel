c9873b01bb7872c0ee7d946ff79def73ffea076c
timebuka <maksim1211ignatenko89@gmail.com>
2023-06-18 00:04:51 +0300

    auth

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // Auth Routes
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
